c09fe7d28ae09e01d2ff825bcaec3962ac029093
Dylan Dewey <dylandewey41@gmail.com>
2020-08-13 17:12:12 -0600

    creating all files and inserting templates to be manipulated

---
+++ db/db.js
@@ -0,0 +1,25 @@
+//Dependencies
+const mySql = require('mysql');
+//Hides password
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "",
+    database: "employee_tracker_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
