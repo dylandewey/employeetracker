faac6de68b99d15b536a8b70f04ca731eaf06ad4
dylandewey <dylandewey41@gmail.com>
2020-08-14 20:55:02 -0600

    adding functions

--- server.js
+++ server.js
@@ -33,12 +33,12 @@ function start() {
                     addEmployee();
                     break;
 
-                case "Find all artists who appear more than once":
-                    multiSearch();
+                case "Add Department":
+                    addDepartment();
                     break;
 
-                case "Find data within a specific range":
-                    rangeSearch();
+                case "Add Role":
+                    addRole();
                     break;
 
                 case "Search for a specific song":
@@ -78,9 +78,9 @@ function addEmployee() {
 
                 function (err) {
                     if (err) throw err;
-
-                    start();
+                    console.table(answer);
                 });
+            start();
         });
 }
 
@@ -88,102 +88,86 @@ function addDepartment() {
     inquirer
         .prompt([
             {
-                name: "firstname",
+                name: "department",
                 type: "input",
-                message: "Employee's First Name:"
+                message: "New Department Name: "
             },
-            {
-                name: "lastname",
-                type: "input",
-                message: "Employee's Last Name:"
-            }
         ])
 
         .then(function (answer) {
-            let query = "INSERT INTO employee set ?";
+            let query = "INSERT INTO department set ?";
             connection.query(query, {
-                first_name: answer.firstname,
-                last_name: answer.lastname,
-                role_id: null,
-                manager_id: null
+                name: answer.department
             },
 
                 function (err) {
                     if (err) throw err;
-
-                    start();
                 });
-        }
 
-function rangeSearch() {
-                inquirer
-                    .prompt([
-                        {
-                            name: "start",
-                            type: "input",
-                            message: "Enter starting position: ",
-                            validate: function (value) {
-                                if (isNaN(value) === false) {
-                                    return true;
-                                }
-                                return false;
-                            }
-                        },
-                        {
-                            name: "end",
-                            type: "input",
-                            message: "Enter ending position: ",
-                            validate: function (value) {
-                                if (isNaN(value) === false) {
-                                    return true;
-                                }
-                                return false;
-                            }
-                        }
-                    ])
-                    .then(function (answer) {
-                        var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-                        connection.query(query, [answer.start, answer.end], function (err, res) {
-                            if (err) throw err;
-                            for (var i = 0; i < res.length; i++) {
-                                console.log(
-                                    "Position: " +
-                                    res[i].position +
-                                    " || Song: " +
-                                    res[i].song +
-                                    " || Artist: " +
-                                    res[i].artist +
-                                    " || Year: " +
-                                    res[i].year
-                                );
-                            }
-                            runSearch();
-                        });
-                    });
+            console.table(answer);
+            start();
+        });
+}
+
+function addRole() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Enter Employee Title",
+                name: "addtitle"
+            },
+            {
+                type: "input",
+                message: "Enter Employee Salary",
+                name: "addsalary"
+            },
+            {
+                type: "input",
+                message: "Enter Employee Department ID",
+                name: "addDeptId"
             }
+        ])
+
+        .then(function (answer) {
+            let query = "INSERT INTO role set ?";
+            connection.query(query, {
+                title: answer.addtitle,
+                salary: answer.addsalary,
+                department_id: answer.addDepId
+            },
+
+                function (err) {
+                    if (err) throw err;
+                });
+
+            console.table(answer);
+            start();
+        });
+}
 
 function songSearch() {
-                inquirer
-                    .prompt({
-                        name: "song",
-                        type: "input",
-                        message: "What song would you like to look for?"
-                    })
-                    .then(function (answer) {
-                        console.log(answer.song);
-                        connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function (err, res) {
-                            if (err) throw err;
-                            console.log(
-                                "Position: " +
-                                res[0].position +
-                                " || Song: " +
-                                res[0].song +
-                                " || Artist: " +
-                                res[0].artist +
-                                " || Year: " +
-                                res[0].year
-                            );
-                            runSearch();
-                        });
-                    });
-            }
+    inquirer
+        .prompt({
+            name: "song",
+            type: "input",
+            message: "What song would you like to look for?"
+        })
+        .then(function (answer) {
+            console.log(answer.song);
+            connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function (err, res) {
+                if (err) throw err;
+                console.log(
+                    "Position: " +
+                    res[0].position +
+                    " || Song: " +
+                    res[0].song +
+                    " || Artist: " +
+                    res[0].artist +
+                    " || Year: " +
+                    res[0].year
+                );
+                runSearch();
+            });
+        });
+}
