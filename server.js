fc9ba8bb420633e41f1a9dd95b064a1b17990a5b
dylandewey <dylandewey41@gmail.com>
2020-08-15 17:42:31 -0600

    adding and changing functions

--- server.js
+++ server.js
@@ -53,35 +53,83 @@ function start() {
 }
 
 function addEmployee() {
-    inquirer
-        .prompt([
-            {
-                name: "firstname",
-                type: "input",
-                message: "Employee's First Name:"
-            },
-            {
-                name: "lastname",
-                type: "input",
-                message: "Employee's Last Name:"
-            }
-        ])
+    connection.query("SELECT * FROM role", function (err, res) {
+        if (err) throw err
+        let myRol = res.map(rol => {
+            return ({
+                name: rol.title,
+                value: rol.id
+            })
+        })
+        connection.query("SELECT * FROM employee", function (err, res) {
+            if (err) throw err
+            let myEmp = res.map(emp => {
+                return ({
+                    name: `${emp.first_name} ${emp.last_name}`,
+                    value: emp.id
+                })
+            })
+            inquirer
+                .prompt([
+                    {
+                        name: "firstname",
+                        type: "input",
+                        message: "Employee's First Name:"
+                    },
+                    {
+                        name: "lastname",
+                        type: "input",
+                        message: "Employee's Last Name:"
+                    },
+                    {
+                        type: "list",
+                        name: "roleId",
+                        message: "Employee's Role:",
+                        choices: myRol
+                    },
+                    {
+                        type: "list",
+                        name: "managerId",
+                        message: "Employee's Manager:",
+                        choices: myEmp,
+                        when: function (answers) {
+                            return myEmp.length > 0
+                        }
+                    }
+                ])
 
-        .then(function (answer) {
-            let query = "INSERT INTO employee set ?";
-            connection.query(query, {
-                first_name: answer.firstname,
-                last_name: answer.lastname,
-                role_id: null,
-                manager_id: null
-            },
+                .then(function (answer) {
+                    if (!answer.managerId) {
+                        let query = "INSERT INTO employee set ?";
+                        connection.query(query, {
+                            first_name: answer.firstname,
+                            last_name: answer.lastname,
+                            role_Id: answer.rollId,
+                        },
 
-                function (err) {
-                    if (err) throw err;
-                    console.table(answer);
-                });
-            start();
-        });
+                            function (err) {
+                                if (err) throw err;
+                                console.table(answer);
+                                start();
+                            });
+                    }
+                    else {
+                        let query = "INSERT INTO employee set ?";
+                        connection.query(query, {
+                            first_name: answer.firstname,
+                            last_name: answer.lastname,
+                            role_Id: answer.rollId,
+                            manager_Id: answer.managerId
+                        },
+                            function (err) {
+                                if (err) throw err;
+                                console.table(answer);
+                                start();
+                            });
+                    }
+                })
+        })
+    })
 }
 
 function addDepartment() {
@@ -102,48 +150,55 @@ function addDepartment() {
 
                 function (err) {
                     if (err) throw err;
-                });
-
-            console.table(answer);
-            start();
-        });
+                    start();
+                })
+        })
 }
 
 function addRole() {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "Enter Employee Title",
-                name: "addtitle"
-            },
-            {
-                type: "input",
-                message: "Enter Employee Salary",
-                name: "addsalary"
-            },
-            {
-                type: "input",
-                message: "Enter Employee Department ID",
-                name: "addDeptId"
-            }
-        ])
+    connection.query("SELECT * FROM department", function (err, res) {
+        if (err) throw err
+        let addDeptId = res.map(dep => {
+            return ({
+                name: dep.name,
+                value: dep.id
+            })
+        })
 
-        .then(function (answer) {
-            let query = "INSERT INTO role set ?";
-            connection.query(query, {
-                title: answer.addtitle,
-                salary: answer.addsalary,
-                department_id: answer.addDepId
-            },
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "Enter Employee Title",
+                    name: "addtitle"
+                },
+                {
+                    type: "input",
+                    message: "Enter Role Salary",
+                    name: "addsalary"
+                },
+                {
+                    type: "input",
+                    message: "Enter Department ID for New Role",
+                    name: "addDeptId"
+                }
+            ])
 
-                function (err) {
-                    if (err) throw err;
-                });
+            .then(function (answer) {
+                let query = "INSERT INTO role set ?";
+                connection.query(query, {
+                    title: answer.addtitle,
+                    salary: answer.addsalary,
+                    department_id: answer.addDepId
+                },
 
-            console.table(answer);
-            start();
-        });
+                    function (err) {
+                        if (err) throw err;
+                        console.table(answer);
+                        start();
+                    })
+            })
+    })
 }
 
 function songSearch() {
